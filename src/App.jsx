86af0a73d7f94f4df1c61c39c75b893a51960e77
Gregory Benner <gregbenner1@gmail.com>
2018-05-30 17:07:44 -0400

    rename to .jsx and add constants file

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import RepoInput from './components/RepoInput'
-
 class App extends Component {
   state = {
     repo: undefined
