a39ce3d1952a6c1642ccab7a506cd215cbf654d1
Gregory Benner <gregbenner1@gmail.com>
2018-05-30 16:56:56 -0400

    get first version of RepoInput working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import RepoInput from './components/RepoInput'
 
 class App extends Component {
+  state = {
+    repo: undefined
+  }
+  repoSet = (repo) => {
+    this.setState({ repo })
+  }
   render () {
     return (
       <div className='App'>
@@ -10,9 +17,7 @@ class App extends Component {
           <img src={logo} className='App-logo' alt='logo' />
           <h1 className='App-title'>Welcome to React</h1>
         </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <RepoInput onSet={this.repoSet} />
       </div>
     )
   }
--- src/components/RepoInput/index.jsx
+++ src/components/RepoInput/index.jsx
@@ -1,16 +1,39 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-// import Input from '@material-ui/core/Input'
+
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+
 import styles from './styles.module.css'
 
 export default class RepoInput extends Component {
   static propTypes = {
     onSet: PropTypes.function // called when the repo is set
   }
+  state = {
+    repo: ''
+  }
+  onChangeInput = ({ target: { value } }) => {
+    this.setState({ repo: value })
+  }
+  onSubmit = (event) => {
+    event.preventDefault()
+    this.onSet(this.state.repo)
+  }
   render () {
     return (
       <div className={styles.container}>
-        <p>Do stuff here...</p>
+        <form onSubmit={this.onSubmit}>
+          <TextField
+            className={styles.input}
+            value={this.state.repo}
+            onChange={this.onChangeInput}
+            label='torvalds/linux'
+          />
+          <Button variant='raised' className={styles.button} onClick={this.onSubmit}>
+            Submit
+          </Button>
+        </form>
       </div>
     )
   }
